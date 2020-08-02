b306b8a1821831a8c8471cbde7258795868ac35f
Angel Fernandez <angel_ferndz@live.com>
2020-08-02 12:46:38 -0400

    First Commit
    Uploading file

---
+++ stock-data-getter.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 19:05:35 2020
+
+@author: angel
+"""
+
+import yfinance as yf
+
+print('-- Stock Data Getter --')
+
+# Gather User input
+ticker = input("> Enter Stock Ticker Symbol: ")
+period = input("> Enter Period [1day, 1mo, max]: ")
+
+# Get Stock Data
+stock = yf.Ticker(ticker).history(period)
+
+print("-----------------------------------------")
+print(stock)
+
+# Save to csv
+save = input("> Save as csv? [y, n]: ")
+
+if save == 'y':
+    stock.to_csv("{}.csv".format(ticker))
